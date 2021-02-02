4a1e941cd6b634b84579e9bac56e1d4ae7468ae2
tan95 <andrew--tan@outlook.com>
2021-02-01 21:50:59 -0800

    General visual improvement

--- src/general_overview.py
+++ src/general_overview.py
@@ -7,6 +7,12 @@ from dash.dependencies import Input, Output
 from shared import app, data_raw
 from viz.us_map import plot_map
 
+state_choice = data_raw.state.unique()
+state_choice.sort()
+
+company_size_choice = data_raw.no_employees.unique()
+company_size_choice.sort()
+
 tab_general_overview_content = dbc.Card(
     dbc.CardBody([
         dbc.Row([
@@ -16,21 +22,31 @@ tab_general_overview_content = dbc.Card(
                     id='overview-state-dropdown',
                     value=[],
                     multi=True,
-                    options=[{'label': col, 'value': col} for col in data_raw.state.sort_values().unique()]),
+                    options=[{'label': col, 'value': col} for col in state_choice]),
+                dbc.FormText('The state or territory where the respondents live in'),
                 html.Br(),
                 dbc.Label('Company Size'),
                 dcc.Dropdown(
                     id='overview-company-size-dropdown',
                     value=[],
                     multi=True,
-                    options=[{'label': col, 'value': col} for col in data_raw.no_employees.unique()]),
+                    options=[{'label': col, 'value': col} for col in company_size_choice]),
+                dbc.FormText('Size of the companies the respondents working for'),
                 html.Br(),
                 dbc.Label('Gender'),
-                dcc.Dropdown(
-                    id='overview-gender-dropdown',
-                    value=[],
-                    multi=True,
-                    options=[{'label': col, 'value': col} for col in data_raw.Gender.unique()]),
+                dcc.RadioItems(
+                    id='overview-gender-radioitems',
+                    options=[
+                        {'label': 'All', 'value': 'All'},
+                        {'label': 'Female', 'value': 'female'},
+                        {'label': 'Male', 'value': 'male'},
+                        {'label': 'Other', 'value': 'Other'}
+                    ],
+                    labelStyle={'display': 'inline-block', 'margin-right': '10px'},
+                    inputStyle={'margin-right': '5px'},
+                    value='All'
+                ),
+                dbc.FormText('Gender of the respondents'),
                 html.Br(),
                 dbc.Label('Age'),
                 dcc.RangeSlider(
@@ -43,6 +59,7 @@ tab_general_overview_content = dbc.Card(
                     },
                     value=[data_raw.Age.min(), data_raw.Age.max()]
                 ),
+                dbc.FormText('Age range of the respondents'),
                 dbc.FormText(id='overview-age-range-text')
             ]), md=2, align='baseline'),
             dbc.Col(dcc.Loading(
@@ -50,12 +67,11 @@ tab_general_overview_content = dbc.Card(
                 type='default',
                 children=html.Iframe(
                     id='iframe-general-overview',
-                    style={'border-width': '0', 'width': '100%', 'height': '70vh'})
-            ), align='center')
+                    style={'border-width': '0', 'width': '100%', 'height': '90vh'})),
+                align='center')
         ])
     ]),
-    className='mt-3',
-)
+    className='mt-3')
 
 
 # Set up callbacks/backend
@@ -63,7 +79,7 @@ tab_general_overview_content = dbc.Card(
     Output('iframe-general-overview', 'srcDoc'),
     Input('overview-state-dropdown', 'value'),
     Input('overview-company-size-dropdown', 'value'),
-    Input('overview-gender-dropdown', 'value'),
+    Input('overview-gender-radioitems', 'value'),
     Input('overview-age-slider', 'value'))
 def plot_general_overview(state, company_size, gender, age_range):
     data = data_raw
@@ -74,7 +90,7 @@ def plot_general_overview(state, company_size, gender, age_range):
     if len(company_size) != 0:
         data = data.query('no_employees == @company_size')
 
-    if len(gender) != 0:
+    if gender.lower() != 'all':
         data = data.query('Gender == @gender')
 
     if len(age_range) != 0:
@@ -83,21 +99,30 @@ def plot_general_overview(state, company_size, gender, age_range):
         data = data.query('Age >= @min_age and Age <= @max_age')
 
     wi = alt.Chart(data).mark_bar().encode(
-        y='work_interfere',
-        x='count()'
+        y=alt.Y('work_interfere', title='Work interference frequency'),
+        x=alt.X('count()', title='Respondent count'),
+        color='Gender'
+    ).properties(
+        title='Does mental health condition interferes work?',
+        height=200
     )
 
     age = alt.Chart(data).mark_bar().encode(
-        y='Age',
-        x='count()'
+        y=alt.Y('Age', title='Age', bin=alt.Bin(maxbins=20)),
+        x=alt.X('count()', title='Respondent count'),
+        color='Gender'
+    ).properties(
+        title='Age distribution',
+        height=200
     )
 
-    wp = alt.Chart(data).mark_bar().encode(
-        y='wellness_program',
-        x='count()'
-    )
-
-    return (wi & age | plot_map(data) & wp).to_html()
+    return (plot_map(data) & (wi | age)).configure_legend(
+        titleFontSize=15,
+        labelFontSize=13
+    ).configure_title(fontSize=18).configure_axis(
+        labelFontSize=13,
+        titleFontSize=13
+    ).to_html()
 
 
 @app.callback(
--- src/top_level_collapse.py
+++ src/top_level_collapse.py
@@ -5,7 +5,7 @@ from dash.dependencies import Input, Output, State
 from shared import app
 
 collapse_button = dbc.Row(
-    dbc.Button('Help', id='collapse-button', color='primary'),
+    dbc.Button('Help', id='top-level-collapse-button', color='primary'),
     no_gutters=True,
     className='ml-auto flex-nowrap mt-5 mt-md-0',
     align='center'
@@ -13,17 +13,23 @@ collapse_button = dbc.Row(
 
 collapse = dbc.Collapse(
     [
-        dbc.Card(dbc.CardBody('This content is hidden in the collapse')),
+        dbc.Card(dbc.CardBody(
+            [
+                html.P('There are 2 tabs for the dashboard. The General Overview tab contains summaries across all '
+                       'companies and all respondents, the Company Overview tab contains summaries mainly focus on '
+                       'statistics across companies only.'),
+                html.P('This message can be collapsed/hidden by clicking the Help button.')
+            ]
+        )),
         html.Br()
     ],
-    id='collapse'
-)
+    id='dashboard_description')
 
 
 @app.callback(
-    Output('collapse', 'is_open'),
-    [Input('collapse-button', 'n_clicks')],
-    [State('collapse', 'is_open')],
+    Output('dashboard_description', 'is_open'),
+    [Input('top-level-collapse-button', 'n_clicks')],
+    [State('dashboard_description', 'is_open')],
 )
 def toggle_collapse(n, is_open):
     if n:
